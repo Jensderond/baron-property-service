6e5d4d2bf4d87868a9ddeb7b70296583f6a8f284
Jens de Rond <jensderond@gmail.com>
2021-12-14 20:07:09 +0100

    Add endpoint for sanity custom input element.

---
+++ src/Controller/SanityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+#[AsController]
+class SanityController extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Property::class);
+    }
+
+    #[Route('/sanity/list-properties', name: 'sanity-list-properties')]
+    public function overviewFilters(): JsonResponse
+    {
+        $em = $this->getEntityManager();
+
+        $allProps = $em->getRepository(Property::class)->findAll();
+
+        $properties = [];
+
+        foreach ($allProps as $prop) {
+            $properties[] = [
+                'id' => $prop->getId(),
+                'title' => $prop->getAddress(),
+            ];
+        }
+
+        return new JsonResponse(['properties' => $properties]);
+    }
+}
