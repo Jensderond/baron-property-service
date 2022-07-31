8446364481b44f798d8f6eb70ede1947df7b3a1c
Jens de Rond <derond@redkiwi.nl>
2022-07-31 16:15:04 +0200

    update symfony/framework-bundle

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,9 +4,7 @@ namespace App;
 
 use function dirname;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
