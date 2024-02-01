cdeb0319003ee0eaa11d300735c85a43ed9776aa
Jens de Rond <derond@redkiwi.nl>
2024-02-01 21:32:35 +0100

    Allow all items for sanity

--- src/Controller/SanityController.php
+++ src/Controller/SanityController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\BogObject;
+use App\Entity\ConstructionNumber;
+use App\Entity\Project;
 use App\Entity\Property;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -27,14 +30,55 @@ class SanityController extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
 
+
+        $allBogs = $em->getRepository(BogObject::class)->createQueryBuilder('p')
+            ->where('p.archived = 0 OR p.archived is null')
+            ->getQuery()
+            ->getResult();
+
+        $allProjects = $em->getRepository(Project::class)->createQueryBuilder('p')
+            ->where('p.archived = 0 OR p.archived is null')
+            ->getQuery()
+            ->getResult();
+
+        $allProjectsNumbers = $em->getRepository(ConstructionNumber::class)->createQueryBuilder('p')
+            ->getQuery()
+            ->getResult();
+
         $properties = [];
 
         foreach ($allProps as $prop) {
             $properties[] = [
-                'id' => $prop->getId(),
+                'id' => $prop->getExternalId(),
                 'title' => $prop->getAddress(),
             ];
         }
+
+        foreach ($allBogs as $prop) {
+            $properties[] = [
+                'id' => $prop->getExternalId(),
+                'title' => $prop->getTitle(),
+            ];
+        }
+
+        foreach ($allProjects as $prop) {
+            $properties[] = [
+                'id' => $prop->getExternalId(),
+                'title' => $prop->getTitle(),
+            ];
+        }
+
+        foreach ($allProjectsNumbers as $prop) {
+            $properties[] = [
+                'id' => $prop->getExternalId(),
+                'title' => $prop->getConstructionType()->getProject()->getTitle() . ' - ' . $prop->getTitle(),
+            ];
+        }
+
+        // sort all properties by title
+        usort($properties, function ($a, $b) {
+            return strcasecmp($a['title'], $b['title']);
+        });
 
         return new JsonResponse(['properties' => $properties]);
     }
