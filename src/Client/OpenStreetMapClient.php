c46e71e35dbc58ba2a9bf9f6f43a975fc1e673bb
Jens de Rond <derond@redkiwi.nl>
2025-01-10 12:08:15 +0100

    Fix types

--- src/Client/OpenStreetMapClient.php
+++ src/Client/OpenStreetMapClient.php
@@ -9,26 +9,26 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class OpenStreetMapClient implements AddressClientInterface
 {
-    public function __construct(private HttpClientInterface $client)
-    {
-        $this->client = $client->withOptions([
-            'base_uri' => 'https://nominatim.openstreetmap.org',
-        ]);
+  public function __construct(private HttpClientInterface $client)
+  {
+    $this->client = $client->withOptions([
+      'base_uri' => 'https://nominatim.openstreetmap.org',
+    ]);
+  }
+
+  public function getLatLngFromAddress(string|int $housenumber, string $street, string $city): string
+  {
+    try {
+      $req = $this->client->request('GET', '/search', [
+        'query' => [
+          'q' => $housenumber . ' ' . $street . ' ' . $city . ' Nederland',
+          'format' => 'json',
+        ],
+      ]);
+    } catch (Exception\TransportExceptionInterface $e) {
+      throw new Error('Something went wrong with the request' . $e);
     }
 
-    public function getLatLngFromAddress(int $housenumber, string $street, string $city): string
-    {
-        try {
-            $req = $this->client->request('GET', '/search', [
-                'query' => [
-                    'q' => $housenumber.' '.$street.' '.$city.' Nederland',
-                    'format' => 'json',
-                ],
-            ]);
-        } catch (Exception\TransportExceptionInterface $e) {
-            throw new Error('Something went wrong with the request'.$e);
-        }
-
-        return $req->getContent();
-    }
+    return $req->getContent();
+  }
 }
